42dcf1c0baed6727ff751ca1c34ab19b6007c420
lnspham <lnspham@gmail.com>
2015-11-23 15:24:42 -0800

    JS Setup. Issue: horizontal slide bar
    The horizontal slide bar showed up when coding the features and playing
    with the width.

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+	/*identify the variables*/
+	$('.readmore').click(showReadMoreContent);
+	$('.readless').click(hideReadMoreContent);
+	$('.learnmore').click(hideLearnMoreContent);
+
+
+	/*provide the actions*/
+
+	//Read More action
+	function showReadMoreContent(event){
+		event.preventDefault();
+		$('#show-this-on-click').slideDown(2000);
+		$('.readless').show();
+		$('.readmore').hide();
+	}
+
+	function hideReadMoreContent(event) {
+		event.preventDefault();
+		$('.readless, p#show-this-on-click').slideUp(2000);
+		$('.readless').hide(2000);
+		$('.readmore').show(3000);
+	}
+
+	//Learn More action
+	function hideLearnMoreContent(event){
+		event.preventDefault();
+		$('#learnmoretext').slideDown(2000);
+		$('.learnmore').hide();
+	}
+
+});
